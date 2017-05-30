c7fa924c9fd9606168154566e841a8ecf1583f60
Jorge Almeida Junior <jorgealmeidajunior@gmail.com>
2017-05-30 15:50:43 -0300

    Correcao do layout quando clico em Quero me cadastrar.

--- clinicas-jee/webcontent/js/index.js
+++ clinicas-jee/webcontent/js/index.js
@@ -1,15 +1,14 @@
 
-$(document).ready(function() {
-    if($('#menu-panel').html() !== "") {
-    	// usuario logou com sucesso, posso exibir o menu
-    	$('#menu-panel').css('left', '0px');
-    } else {
-    	// usuario nao esta logado, aumento a area do content pane
-    	$('#content-pane').css('margin-left', '0px');
-    }
-});
-
 var indexController = {
+	atualizarMenuPanel : function() {
+		if($('#menu-panel').html() !== "") {
+	    	// usuario logou com sucesso, posso exibir o menu
+	    	$('#menu-panel').css('left', '0px');
+	    } else {
+	    	// usuario nao esta logado, aumento a area do content pane
+	    	$('#content-pane').css('margin-left', '0px');
+	    }
+	},
 	
 	doAfterTryLogin : function() {
 		if($('#menu-panel').html().toString().replace(/ /g,'') !== "") {
@@ -28,5 +27,8 @@ var indexController = {
 			"margin-left" : "-=160px"
 		}, 1500, function() { });
 	}
-
 };
+
+$(document).ready(function() {
+	indexController.atualizarMenuPanel();
+});
